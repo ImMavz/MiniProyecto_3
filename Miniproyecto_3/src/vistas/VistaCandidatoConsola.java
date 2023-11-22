79cf90427c086519b1593e0b541a42b381d5b953
Samu ESRI <escobar.samuel@correounivalle.edu.co>
2023-11-21 20:17:04 -0500

    Agregre vistas

---
+++ Miniproyecto_3/src/vistas/VistaCandidatoConsola.java
@@ -0,0 +1,48 @@
+package vistas;
+
+public class VistaCandidatoConsola implements VistaCandidato {
+
+    @Override
+    public void iniciar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciar'");
+    }
+
+    @Override
+    public String getNombre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNombre'");
+    }
+
+    @Override
+    public String getPromesas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPromesas'");
+    }
+
+    @Override
+    public String getCedula() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCedula'");
+    }
+
+    @Override
+    public int getVotos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVotos'");
+    }
+
+    @Override
+    public void getIdeologia() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getIdeologia'");
+    }
+
+    @Override
+    public void getCiudad() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCiudad'");
+    }
+
+    
+}
