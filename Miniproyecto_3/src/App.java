03fd71e6468d500d8bfaec10721e81d9f65e6dab
Joseph <144748605+ImMavz@users.noreply.github.com>
2023-11-29 16:31:16 -0500

    Se combinan las dos vistas
    Se combinan la vista de consola y GUI y la vista GUI es funcional

--- Miniproyecto_3/src/App.java
+++ Miniproyecto_3/src/App.java
@@ -1,8 +1,21 @@
+import controlador.ControladorGUI;
 import vistas.VistaCandidatoConsola;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
+        ControladorGUI controlador = new ControladorGUI();
         VistaCandidatoConsola vistaCandidatoConsola = new VistaCandidatoConsola();
+
+        controlador.iniciarVistaCandidatoGUI();
+        controlador.iniciarVistaEliminarCandidato();
+        controlador.iniciarVistaListaCandidato();
+        controlador.iniciarVistaVotosCandidatos();
+        controlador.iniciarVistaCandidatoGanador();
+        controlador.iniciarVistaCiudadesCandidato();
+        controlador.iniciarVistaPartidosCandidato();
+        controlador.iniciarVistaCrearCandidato();
+
         vistaCandidatoConsola.iniciar();
 
     }
